767c40759ef081c909f9e34c214520b1c27df49a
aleks <aleksiya.solovyova@student.kdg.be>
2025-05-28 04:33:09 +0200

    project done

---
+++ tarot-read/src/main/java/com/example/tarotread/TarotReadApplication.java
@@ -0,0 +1,13 @@
+package com.example.tarotread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarotReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TarotReadApplication.class, args);
+    }
+
+}
---
+++ tarot-read/src/test/java/com/example/tarotread/TarotReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarotread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class TarotReadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
